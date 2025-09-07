1fa081ac2413d8573c5585a63976747d4e792a14
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-07 23:52:11 +0330

    add query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NotFound from './pages/NotFound';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <BrowserRouter basename="/cores">
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter basename="/cores">
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
 
