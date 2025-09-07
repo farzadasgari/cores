c32bca5e7e402a57a706f52b80bf7b9c666dd3a9
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-07 23:40:21 +0330

    initializing router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 function App() {
     return (
         <BrowserRouter basename="/cores">
             <Routes>
-                <Route path="*" element={<Index />} />
+                <Route path="/" element={<Index />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
