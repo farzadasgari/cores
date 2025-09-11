1414bed7e9e8c35aac6b278c9a7ec1108bd4df81
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-11 10:46:47 +0330

    add mobile sidebar icon button

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Menu } from "lucide-react"
+
 export const DashboardHeader = () => {
     return (
         <header className="fixed top-0 left-0 right-0 h-16 z-50 bg-slate-100 border-b border-rose-600 hover:border-rose-400 duration-300">
             <div className="flex items-center justify-between h-full px-4">
-                
+                <Button variant="ghost" size="sm" className="md:hidden">
+                    <Menu className="h-5 w-5" />
+                </Button>
             </div>
         </header>
     )
