e88c63e75034490505f289e5c62f23a635a80d55
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-11 10:58:59 +0330

    add theme toggle button

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button"
-import { Menu } from "lucide-react"
+import { Menu, Moon, Bell } from "lucide-react"
 
 export const DashboardHeader = () => {
     return (
         <header className="fixed top-0 left-0 right-0 h-16 z-50 bg-slate-100 border-b border-rose-600 hover:border-rose-400 duration-300">
             <div className="flex items-center justify-between h-full px-4">
-                <Button variant="ghost" size="sm" className="md:hidden">
-                    <Menu className="h-5 w-5" />
-                </Button>
+                {/* Left Section */}
+                <div className="flex items-center gap-4">
+                    <Button variant="ghost" size="sm" className="md:hidden">
+                        <Menu className="h-5 w-5" />
+                    </Button>
+                </div>
+
+                {/* Right Section */}
+                <div className="flex items-center gap-2">
+                    <Button variant="ghost" size="sm" >
+                        <Moon className="h-5 w-5" />
+                    </Button>
+                </div>
             </div>
         </header>
     )
