017cf0fab65eab1a18e421ab04db86e5d583557e
Farzad Asgari <khufarzadasgari@gmail.com>
2025-09-14 21:43:34 +0330

    initializing sidebar

---
+++ src/components/DashboardSidebar.tsx
@@ -0,0 +1,7 @@
+export const DashboardSidebar = () => {
+    return (
+        <aside className="fixed left-0 top-16 z-50 h-[calc(100vh-4rem)] bg-background border-r border-rose-600 hover:border-rose-400 duration-300 overflow-hidden w-64">
+            
+        </aside>
+    );
+};
