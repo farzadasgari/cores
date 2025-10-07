9128f0d5ce08b7f178a1e45b1f90c33a323ac305
Farzad Asgari <khufarzadasgari@gmail.com>
2025-10-07 10:28:44 +0330

    add welcome section to index

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -7,7 +7,20 @@ export const DashboardContent = ({ currentPage }: DashboardContentProps) => {
         switch (currentPage) {
             default:
                 return (
-                    <div className="space-y-6">This is dashboard content</div>
+                    <div className="space-y-6">
+                        {/* Welcom Section */}
+                        <div className="bg-gradient-to-r from-rose-500/90 to-slate-200/90 rounded-2xl p-8 text-white relative overflow-hidden">
+                            <div className="relative z-10">
+                                <h1 className="text-3xl font-bold mb-2">
+                                    Welcome back, John!👋🏻
+                                </h1>
+                                <p className="text-white/90 text-lg">
+                                    Here's what's happening with your business
+                                    today.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 );
         }
     };
