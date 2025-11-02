ca8e9d13fd1cd222943f1cfce28fd99a96149355
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-02 12:22:33 +0330

    add render section to settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -4,6 +4,31 @@ import { SettingsNavigation } from '@/components/Settings/SettingsNavigation';
 
 export const Settings = () => {
     const [activeSection, setActiveSection] = useState<string>('general');
+
+    const renderActiveSection = () => {
+        switch (activeSection) {
+            case "general":
+                return <span>General Section</span>
+            case "user-permissions":
+                return <span>Users and Permissions Section</span>
+            case "integrations":
+                return <span>Integrations Section</span>
+            case "notifications":
+                return <span>Notifications Section</span>
+            case "security":
+                return <span>Security Section</span>
+            case "appearance":
+                return <span>Appearance Section</span>
+            case "billing":
+                return <span>Billing Section</span>
+            case "performance":
+                return <span>Performance Section</span>
+            case "advanced":
+                return <span>Advanced Section</span>
+            case "audit-log":
+                return <span>Audit-Log Section</span>
+        }
+    }
     return (
         <div className="space-y-6 p-4 md:p-6 overflow-hidden max-w-full">
             <SettingsHeader />
@@ -13,6 +38,12 @@ export const Settings = () => {
                         activeSection={activeSection}
                         onSectionChange={setActiveSection}
                     />
+                </div>
+
+                <div className='flex-1 min-w-0'>
+                    <div className='bg-card rounded-lg border hover:border-primary/30 duration-300 p-6 min-h-[600px]'>
+                        {renderActiveSection()}
+                    </div>
                 </div>
             </div>
         </div>
