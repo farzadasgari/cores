ccddcc4f998b883aa9b813d86b92d88c2e093559
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-03 09:44:36 +0330

    add card ui to settings general section

--- src/components/Settings/SettingsGeneral.tsx
+++ src/components/Settings/SettingsGeneral.tsx
@@ -1,5 +1,26 @@
+import { Card, CardHeader, CardTitle ,CardContent } from "@/components/ui/card"
+
 export const SettingsGeneral = () => {
     return (
-        <>General Settings Page</>
+        <div className="space-y-6">
+            <div>
+                <h2 className="text-lg font-semibold mb-2">General Settings</h2>
+                <p className="text-muted-foreground">
+                    Configure basic organization settings and preferences.
+                </p>
+            </div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Organization Profile</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
